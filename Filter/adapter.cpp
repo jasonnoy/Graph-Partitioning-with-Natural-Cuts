d4639d6a001cb89bee4631e62596f53084981cac
junhui <17667520168@163.com>
2021-09-28 17:47:14 +0800

    amends

--- Filter/adapter.cpp
+++ Filter/adapter.cpp
@@ -1,7 +1,7 @@
 #include "adapter.h"
 #include <iostream>
 
-G_Edge sw_edge_adapter( link_info_t linkInfo, unsigned int nid, map<NodeID, NodeID> id_map) {
+G_Edge sw_edge_adapter( link_info_t linkInfo, unsigned int nid, map<NodeID, NodeID> &id_map) {
     NodeID oid = linkInfo.sw_link_id, source = id_map[linkInfo.start_node_id], target = id_map[linkInfo.end_node_id];
     if (nid == 100) {
         cout<<"oid: "<<oid<<"source: "<<linkInfo.start_node_id<<"target: "<<linkInfo.end_node_id<<endl;
