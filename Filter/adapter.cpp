75d99928a2be2a81811db6191c21518671f27aaa
junhui <17667520168@163.com>
2021-09-28 16:47:40 +0800

    amends

--- Filter/adapter.cpp
+++ Filter/adapter.cpp
@@ -2,6 +2,9 @@
 
 G_Edge sw_edge_adapter( link_info_t linkInfo, unsigned int nid, map<NodeID, NodeID> &id_map) {
     NodeID oid = linkInfo.sw_link_id, source = id_map[linkInfo.start_node_id], target = id_map[linkInfo.end_node_id];
+    if (nid == 0) {
+        cout<<"oid: "<<oid<<"source: "<<source<<"target: "<<target<<endl;
+    }
     G_Edge edge(source, target, nid, oid);
     return edge;
 }
