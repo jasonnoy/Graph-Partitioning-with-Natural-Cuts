68b95c157fbb8de2e081ad1c2657292d21bdff9e
junhui <17667520168@163.com>
2021-09-28 17:31:18 +0800

    amends

--- Filter/adapter.cpp
+++ Filter/adapter.cpp
@@ -1,12 +1,11 @@
 #include "adapter.h"
 #include <iostream>
 
-G_Edge sw_edge_adapter( link_info_t linkInfo, unsigned int nid, map<NodeID, NodeID> *id_map) {
-    auto map = *id_map;
-    NodeID oid = linkInfo.sw_link_id, source = map[linkInfo.start_node_id], target = map[linkInfo.end_node_id];
+G_Edge sw_edge_adapter( link_info_t linkInfo, unsigned int nid, map<NodeID, NodeID> id_map) {
+    NodeID oid = linkInfo.sw_link_id, source = id_map[linkInfo.start_node_id], target = id_map[linkInfo.end_node_id];
     if (nid == 100) {
         cout<<"oid: "<<oid<<"source: "<<linkInfo.start_node_id<<"target: "<<linkInfo.end_node_id<<endl;
-        cout<<"source id: "<<map[linkInfo.start_node_id]<<" target id: "<<map[linkInfo.end_node_id]<<endl;
+        cout<<"source id: "<<id_map[linkInfo.start_node_id]<<" target id: "<<id_map[linkInfo.end_node_id]<<endl;
     }
     G_Edge edge(source, target, nid, oid);
     return edge;
--- Filter/adapter.h
+++ Filter/adapter.h
@@ -7,7 +7,7 @@
 #ifndef GRAPH_PARTITIONING_WITH_NATURAL_CUTS_ADAPTER_H
 #define GRAPH_PARTITIONING_WITH_NATURAL_CUTS_ADAPTER_H
 
-G_Edge sw_edge_adapter( link_info_t linkInfo, EdgeID oid, map<NodeID, NodeID> *id_map );
+G_Edge sw_edge_adapter( link_info_t linkInfo, EdgeID oid, map<NodeID, NodeID> id_map );
 G_Node sw_node_adapter( node_info_t nodeInfo, NodeID oid );
 
 #endif //GRAPH_PARTITIONING_WITH_NATURAL_CUTS_ADAPTER_H
