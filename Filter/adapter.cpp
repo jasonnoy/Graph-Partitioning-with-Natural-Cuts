faa87455286a22bce9b737f8d7122bf5bc53988e
junhui <17667520168@163.com>
2021-09-28 16:14:37 +0800

    amends

--- Filter/adapter.cpp
+++ Filter/adapter.cpp
@@ -1,14 +1,14 @@
 #include "adapter.h"
 
-G_Edge sw_edge_adapter( link_info_t linkInfo, unsigned int oid, map<NodeID, NodeID> &id_map) {
-    NodeID id = linkInfo.sw_link_id, source = id_map[linkInfo.start_node_id], target = id_map[linkInfo.end_node_id];
-    G_Edge edge(id, source, target, oid);
+G_Edge sw_edge_adapter( link_info_t linkInfo, unsigned int nid, map<NodeID, NodeID> &id_map) {
+    NodeID oid = linkInfo.sw_link_id, source = id_map[linkInfo.start_node_id], target = id_map[linkInfo.end_node_id];
+    G_Edge edge(nid, source, target, oid);
     return edge;
 }
 
-G_Node sw_node_adapter( node_info_t nodeInfo, unsigned int oid) {
-    NodeID id = nodeInfo.sw_node_id;
-    G_Node node(id, oid);
+G_Node sw_node_adapter( node_info_t nodeInfo, unsigned int nid) {
+    NodeID oid = nodeInfo.sw_node_id;
+    G_Node node(nid, oid);
     return node;
 }
 
