7fe324bd383cf074cc8e88ade696a914186d557b
junhui <17667520168@163.com>
2021-09-29 11:53:08 +0800

    amends

--- Filter/G_Graph.cpp
+++ Filter/G_Graph.cpp
@@ -64,7 +64,7 @@ void G_Graph::read_graph(string co_path, string gr_path){
         }
         if (edge.get_target()==0) {
             cout<<"target 0, counter: "<<counter<<" id: "<<edge.get_id()<<endl;
-            cout<<"edge list back id: "<<edge_list.back().get_id()<<endl;
+            cout<<"edge list back id: "<<edge_list.back().get_id()<<" source: "<<edge_list.back().get_source()<<endl;
             cout<<"adj list[0] source id: "<<node_list[edge.get_id()].get_adj_list()[0]->get_source()<<endl;
         }
         counter++;
