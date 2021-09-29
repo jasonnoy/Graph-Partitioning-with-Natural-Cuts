fb0d2b318efd9bd341a5b2290cfa5844ba54a028
junhui <17667520168@163.com>
2021-09-29 10:58:45 +0800

    amends

--- Filter/G_Graph.cpp
+++ Filter/G_Graph.cpp
@@ -160,8 +160,8 @@ void G_Graph::dfs_tree( NodeID start, vector<bool>& edge_removed, NodeSize size_
 			NodeID n = node_stack.back();
 			node_stack.pop_back();
 
-            cout<<"cur node id: "<<this->node_list[n].get_id()<<" adj size:"<<this->node_list[n].get_adj_list().size()<<endl;
-            cout<<"source: "<<this->node_list[n].get_adj_list()[0].get_source()<<"target: "<<this->node_list[n].get_adj_list()[0].get_target();
+            cout<<"cur node id: "<<this->node_list[n].get_id()<<" adj size:"<<this->node_list[n]->get_adj_list().size()<<endl;
+            cout<<"source: "<<this->node_list[n].get_adj_list()[0].get_source()<<"target: "<<this->node_list[n]->get_adj_list()[0].get_target();
 
 			//map<NodeID, G_Edge*>::const_iterator it = this->node_list[n].get_adj_list().begin();
 			vector<G_Edge*>::const_iterator it = this->node_list[n].get_adj_list().begin();
