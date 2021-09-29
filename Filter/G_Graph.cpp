a356d62c235bcf12e73b56ab979054e059ab689b
junhui <17667520168@163.com>
2021-09-29 13:39:43 +0800

    amends

--- Filter/G_Graph.cpp
+++ Filter/G_Graph.cpp
@@ -61,7 +61,6 @@ void G_Graph::read_graph(string co_path, string gr_path){
             cout<<"source 0, counter: "<<counter<<" id: "<<edge.get_id()<<endl;
             cout<<"edge list back id: "<<edge_list.back().get_id()<<endl;
             cout<<"adj list[0] target id: "<<node_list[0].get_adj_list()[0]->get_target()<<endl;
-            cout<<"node 0: id: "<<node_list[0].get_id()<<endl;
         }
         if (edge.get_target()==0) {
             cout<<"target 0, counter: "<<counter<<" id: "<<edge.get_id()<<endl;
@@ -69,6 +68,7 @@ void G_Graph::read_graph(string co_path, string gr_path){
             cout<<"adj list[target] target id: "<<node_list[edge_list.back().get_source()].get_adj_list()[0]->get_target()<<endl;
         }
         if (counter == 930000) {
+            cout<<"node 0: id: "<<node_list[0].get_id()<<"oid: "<<node_list[0].get_origin_id()<<endl;
             cout<<"adj list[0] target id: "<<node_list[0].get_adj_list()[0]->get_target()<<endl;
         }
         counter++;
