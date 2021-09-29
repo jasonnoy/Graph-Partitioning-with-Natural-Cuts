920302521884a18086b253b3aefba3cdc0ac0e0e
junhui <17667520168@163.com>
2021-09-29 15:28:21 +0800

    amends

--- Filter/G_Graph.cpp
+++ Filter/G_Graph.cpp
@@ -63,11 +63,11 @@ void G_Graph::read_graph(string co_path, string gr_path){
             cout<<"edge list back id: "<<edge_list.back().get_id()<<endl;
             cout<<"adj list[0] target id: "<<node_list[0].get_adj_list()[0]->get_target()<<" address: "<<&node_list[0].get_adj_list()[0]<<endl;
         }
-//        if (edge.get_target()==0) {
-//            cout<<"target 0, counter: "<<counter<<" id: "<<edge.get_id()<<endl;
-//            cout<<"edge list back id: "<<edge_list.back().get_id()<<" source: "<<edge_list.back().get_source()<<endl;
-//            cout<<"adj list[target] target id: "<<node_list[edge_list.back().get_source()].get_adj_list()[0]->get_target()<<endl;
-//        }
+        if (edge.get_target()==10000) {
+            cout<<"target 0, counter: "<<counter<<" id: "<<edge.get_id()<<endl;
+            cout<<"edge list back id: "<<edge_list.back().get_id()<<" source: "<<edge_list.back().get_source()<<endl;
+            cout<<"adj list[target] target id: "<<node_list[edge_list.back().get_source()].get_adj_list()[0]->get_target()<<endl;
+        }
 //        if (counter == 930000) {
 //            cout<<"node 0: id: "<<node_list[0].get_id()<<"oid: "<<node_list[0].get_origin_id()<<endl;
 //            cout<<"adj list[0] target id: "<<node_list[0].get_adj_list()[0]->get_target()<<" address: "<<&node_list[0].get_adj_list()[0]<<endl;
