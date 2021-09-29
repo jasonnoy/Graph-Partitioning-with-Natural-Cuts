dfdf1e578e98e76a06e7b4468c4b3ea851b8ff1c
junhui <17667520168@163.com>
2021-09-29 11:02:06 +0800

    amends

--- Filter/adapter.cpp
+++ Filter/adapter.cpp
@@ -3,6 +3,9 @@
 
 G_Edge sw_edge_adapter( link_info_t linkInfo, EdgeID nid) {
     NodeID oid = linkInfo.sw_link_id, source = linkInfo.start_node_id, target = linkInfo.end_node_id;
+    if (nid < 100) {
+        cout<<"id: "<<nid<<" source: "<<source<<" target: "<<target<<endl;
+    }
     G_Edge edge(source, target, nid, oid);
     return edge;
 }
