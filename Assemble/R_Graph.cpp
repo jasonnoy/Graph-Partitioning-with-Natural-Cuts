bb2420594adaca44b0d906423fecd395f6abad5d
junhui <17667520168@163.com>
2021-10-13 14:31:16 +0800

    bug fixing

--- Assemble/R_Graph.cpp
+++ Assemble/R_Graph.cpp
@@ -97,7 +97,8 @@ EdgeWeight R_Graph::write_result( vector< vector<NodeID> >& result, vector< vect
 			vector< NodeID >::const_iterator nit = cit->begin();
 			final_result[i].reserve( 100 * cit->size() );
 			for(; nit != cit->end(); nit++){
-				final_result[i].insert(final_result[i].end(), id_map[*nit].begin(), id_map[*nit].end() );
+                cout<<"id_map[*nit] size: "<<id_map[*nit]<<endl;
+//				final_result[i].insert(final_result[i].end(), id_map[*nit].begin(), id_map[*nit].end() );
 			}
 		}
 
