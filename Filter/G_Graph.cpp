fa1daced36cab9a9f592be7468732cabb6aa48b9
junhui <17667520168@163.com>
2021-10-15 14:51:55 +0800

    fix bugs

--- Filter/G_Graph.cpp
+++ Filter/G_Graph.cpp
@@ -96,12 +96,12 @@ void G_Graph::read_graph( const vector<NodeID>& nodes, const vector<vector<NodeI
                 break;
             }
         }
-//        G_Edge newEdge(edge_list[i].get_target(), edge_list[i].get_source(), eid);
-//        edge_list.push_back(newEdge);
-//        node_list[newEdge.get_source()].get_adj_list().push_back(&edge_list.back());
-//        sym_id[i] = eid;
-//        sym_id[eid] = i;
-//        eid++;
+        G_Edge newEdge(edge_list[i].get_target(), edge_list[i].get_source(), eid);
+        edge_list.push_back(newEdge);
+        node_list[newEdge.get_source()].get_adj_list().push_back(&edge_list.back());
+        sym_id[i] = eid;
+        sym_id[eid] = i;
+        eid++;
     }
     for (int i = 0; i < 20; i++) {
         cout<<"sym_id"<<i<<": "<<sym_id[i]<<endl;
