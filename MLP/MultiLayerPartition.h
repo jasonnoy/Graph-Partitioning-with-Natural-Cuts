9d2f75bca598904b2bfe82c5dc918b2ca4d54398
junhui <17667520168@163.com>
2021-10-18 23:23:24 +0800

    fix bug

--- MLP/MultiLayerPartition.h
+++ MLP/MultiLayerPartition.h
@@ -12,7 +12,7 @@
 #include "Filter.h"
 #include "GraphPrinter.h"
 #include "Preprocess.h"
-#include "PostProgress.h"
+//#include "PostProgress.h"
 #include <string>
 #include <iostream>
 #include <stdlib.h>
