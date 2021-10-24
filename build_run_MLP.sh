4caee49a1f8cccdf771cf8ea7dc997f146f43b40
junhui <17667520168@163.com>
2021-10-24 16:07:50 +0800

    prune and add sh entry

--- build_run_MLP.sh
+++ build_run_MLP.sh
@@ -9,5 +9,13 @@ else
   mkdir "$date_str"
 fi
 
+if [ -f "./logs/$date_str/$time_str.log" ]
+then
+  echo "log ./$date_str/$time_str.log exists, creating $time_str _1"
+  touch ./"$date_str"/"$time_str"_1.log || exit
+else
+  echo "output to ./$date_str/$time_str.log"
+  touch ./"$date_str"/"$time_str".log || exit
+fi
 g++ ./Common/Utility.cpp ./MLP/Filter.cpp ./MLP/Assembly.cpp ./MLP/MultiLayerPartition.cpp ./PUNCH/Filter/G_Graph.cpp ./PUNCH/Assemble/A_Graph.cpp -o ./bin/MLP -std=c++11 > ./logs/"$date_str"/"$time_str".log 2>&1
 ./bin/MLP ./data/paras.txt ./data/nodes_all ./data/links ./result/ >> ./logs/"$date_str"/"$time_str".log 2>&1
