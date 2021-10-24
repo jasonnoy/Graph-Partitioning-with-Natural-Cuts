42c5c2505dd3e6ca0a7f4162dabab42ea9b07053
junhui <17667520168@163.com>
2021-10-24 17:23:41 +0800

    prune and add sh entry

--- build_run_MLP.sh
+++ build_run_MLP.sh
@@ -30,3 +30,4 @@ fi
 g++ ./MLP/MultiLayerPartition.cpp ./MLP/Assembly.cpp ./MLP/Filter.cpp ./MLP/AdaptivePrinter.cpp ./MLP/Preprocess.cpp ./MLP/GraphPrinter.cpp ./Common/Utility.cpp ./PUNCH/Filter/G_Graph.cpp ./PUNCH/Assemble/A_Graph.cpp -o ./bin/MLP -std=c++11 > ./logs/"$date_str"/"$time_str".log 2>&1
 
 ./bin/MLP ./data/paras.txt ./data/nodes_all ./data/links ./result/ >> ./logs/"$date_str"/"$time_str".log 2>&1
+echo "Finished"
