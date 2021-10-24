da4635045fd42ec404f1e87ea0040278f750e572
junhui <17667520168@163.com>
2021-10-24 16:10:14 +0800

    prune and add sh entry

--- build_run_MLP.sh
+++ build_run_MLP.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 date_str=$(date +%Y%m%d)
 time_str=$(date +%H%M%S)
-if [ -d "$date_str" ]
+
+if [ -d logs ]
 then
-  echo "dir $date_str exists"
+  echo "dir logs exists"
 else
-  echo "creating dir $date_str"
-  mkdir "$date_str"
+  echo "creating dir logs"
+  mkdir "logs"
+fi
+
+if [ -d ./log/"$date_str" ]
+then
+  echo "dir ./logs/$date_str exists"
+else
+  echo "creating dir ./logs/$date_str"
+  mkdir "./logs/$date_str"
 fi
 
 if [ -f "./logs/$date_str/$time_str.log" ]
 then
-  echo "log ./$date_str/$time_str.log exists, creating $time_str _1"
-  touch ./"$date_str"/"$time_str"_1.log || exit
+  echo "log .logs/$date_str/$time_str.log exists, creating $time_str _1"
+  touch .logs/"$date_str"/"$time_str"_1.log || exit
 else
-  echo "output to ./$date_str/$time_str.log"
-  touch ./"$date_str"/"$time_str".log || exit
+  echo "output to .logs/$date_str/$time_str.log"
+  touch .logs/"$date_str"/"$time_str".log || exit
 fi
 g++ ./Common/Utility.cpp ./MLP/Filter.cpp ./MLP/Assembly.cpp ./MLP/MultiLayerPartition.cpp ./PUNCH/Filter/G_Graph.cpp ./PUNCH/Assemble/A_Graph.cpp -o ./bin/MLP -std=c++11 > ./logs/"$date_str"/"$time_str".log 2>&1
 ./bin/MLP ./data/paras.txt ./data/nodes_all ./data/links ./result/ >> ./logs/"$date_str"/"$time_str".log 2>&1
