fba615914eb6b2abe1c4b8f325e8101ae4b8e78a
junhui <17667520168@163.com>
2021-10-26 14:37:59 +0800

    test

--- PUNCH/Assemble/Logic_Edge.h
+++ PUNCH/Assemble/Logic_Edge.h
@@ -29,7 +29,7 @@ public:
 
 	bool operator < ( const Logic_Edge &le ){
 
-		return this->score < le.score; // 改为最小堆
+		return this->score > le.score; // 改为最小堆
 	}
 
 	NodeID source; //only contracted node ID
