de4ddf7b14d127ea64b4bec2f8cb82a5e0f3b6d8
junhui <17667520168@163.com>
2021-11-02 14:35:40 +0800

    parallel test

--- MLP/MultiLayerPartition.cpp
+++ MLP/MultiLayerPartition.cpp
@@ -192,7 +192,7 @@ void MultiLayerPartition::MLP() {
         atomic<int> process_count(0);
         vector<thread> ths;
         for (int i = 0; i < cells.size(); i++) {
-            ths.push_back(thread{&MultiLayerPartition::dealCell, this, l, cur_layer, cells[i], cellCount, edgeCount, void_nodes, process_count});
+            ths.push_back(thread(&MultiLayerPartition::dealCell, this, l, cur_layer, cells[i], cellCount, edgeCount, void_nodes, process_count));
 //            ths.push_back(thread(dealCell, i, l, cur_layer, ref(cells[i]), ref(cellCount), ref(edgeCount), ref(void_nodes), ref(process_count), ref(graph_edges), outPath, nodeNum, U, C, FI, M, L));
         }
         for (int i = 0; i < cells.size(); i++){
