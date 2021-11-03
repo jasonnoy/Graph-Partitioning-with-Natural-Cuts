8a6db375d2b51c96298bd5b18224fa4d76c9976d
junhui <17667520168@163.com>
2021-11-03 11:59:48 +0800

    parallel test

--- MLP/MultiLayerPartition.cpp
+++ MLP/MultiLayerPartition.cpp
@@ -11,8 +11,8 @@ mutex m_lock, file_lock;
 atomic<int> process_count(0);
 
 const unsigned int hardware_threads = thread::hardware_concurrency();
-const int thread_limit = hardware_threads / 2;
-//const int thread_limit = 1;
+//const int thread_limit = hardware_threads / 2;
+const int thread_limit = 16;
 
 // Parallel function
 void dealCell(int processId, int l, string cur_layer, vector<unsigned int> &cell, atomic<int> &cellCount, atomic<int> &edgeCount, vector <NodeID> &void_nodes, const vector<vector<unsigned int>>& graph_edges, const string outPath, const unsigned int nodeNum, const int U, const int C, const int FI, const int M, const int L) {
