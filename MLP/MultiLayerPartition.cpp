8fc0ea681ded287bac62982b034c3d3a2a9d0d11
junhui <17667520168@163.com>
2021-11-03 15:27:52 +0800

    default thread num:1

--- MLP/MultiLayerPartition.cpp
+++ MLP/MultiLayerPartition.cpp
@@ -11,7 +11,7 @@ atomic<int> process_count(0);
 
 const unsigned int hardware_threads = thread::hardware_concurrency();
 //const int thread_limit = hardware_threads / 2;
-const int thread_limit = 1;
+int thread_limit = 1;
 
 // Parallel function
 void dealCell(int processId, int l, string cur_layer, vector<unsigned int> &cell, atomic<int> &cellCount, atomic<int> &edgeCount, vector <NodeID> &void_nodes, const vector<vector<unsigned int>>& graph_edges, const string outPath, const unsigned int nodeNum, const int U, const int C, const int FI, const int M, const int L) {
