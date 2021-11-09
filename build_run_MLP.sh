29e88914db68f69bfe173c4c6a818fdd0966e2cd
junhui <17667520168@163.com>
2021-11-09 13:44:41 +0800

    change to 32 thread

--- build_run_MLP.sh
+++ build_run_MLP.sh
@@ -45,7 +45,7 @@ fi
 
 g++ ./MLP/MultiLayerPartition.cpp -g -lpthread ./MLP/Assembly.cpp ./MLP/Filter.cpp ./MLP/AdaptivePrinter.cpp ./MLP/Preprocess.cpp ./MLP/GraphPrinter.cpp ./Common/Utility.cpp ./PUNCH/Filter/G_Graph.cpp ./PUNCH/Assemble/A_Graph.cpp -o ./bin/MLP -std=c++11 > ./logs/"$date_str"/"$time_str".log 2>&1
 
-./bin/MLP ./data/paras.txt /data/cris.su/code/ch-routing-demo/data/china/nodes /data/cris.su/code/ch-routing-demo/data/china/links ./result/ 40 >> ./logs/"$date_str"/"$time_str".log 2>&1
+./bin/MLP ./data/paras.txt /data/cris.su/code/ch-routing-demo/data/china/nodes /data/cris.su/code/ch-routing-demo/data/china/links ./result/ 32 >> ./logs/"$date_str"/"$time_str".log 2>&1
 
 # ./bin/MLP ./data/paras.txt ./data/nodes_all ./data/links ./result/ 1 >> ./logs/"$date_str"/"$time_str".log 2>&1
 
