7811488a2bda1f7a57bd37873081013947d70002
junhui <17667520168@163.com>
2021-11-15 16:55:09 +0800

    test

--- build_run_MLP.sh
+++ build_run_MLP.sh
@@ -48,6 +48,6 @@ g++ ./MLP/MultiLayerPartition.cpp -g -lpthread ./MLP/Assembly.cpp ./MLP/Filter.c
 
 #./bin/MLP ./data/paras.txt /data/cris.su/code/ch-routing-demo/data/china/nodes /data/cris.su/code/ch-routing-demo/data/china/links ./result/ 32 >> ./logs/"$date_str"/"$time_str".log 2>&1
 
- ./bin/MLP ./data/paras_beijing.txt ./data/nodes_all ./data/links ./result/beijing/ 32 >> ./logs/"$date_str"/"$time_str".log 2>&1
+ ./bin/MLP ./data/paras_beijing.txt ./data/nodes_all ./data/links ./result/beijing/ 4 >> ./logs/"$date_str"/"$time_str".log 2>&1
 
 echo "Finished"
