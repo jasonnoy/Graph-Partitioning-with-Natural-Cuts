33a90f8c11759973155a5da2c8438a05453f7fe7
junhui <17667520168@163.com>
2021-11-19 14:15:35 +0800

    test

--- PUNCH/Filter/G_Graph.cpp
+++ PUNCH/Filter/G_Graph.cpp
@@ -2306,9 +2306,9 @@ void G_Graph::link_component( vector<edge_cncted_comp>& component_tree, map<Node
         if (search_pos < component_tree.size()) {
             for (NodeID chl_id : component_tree[search_pos].children){
                 NodeID new_search_pos = chl_id;
-                if (searched[new_search_pos])
-                    continue;
-                this->link_component( component_tree, comp_cnodes_to_pos, new_search_pos, search_pos, searched );
+                cout<<new_search_pos<<":"<<searched<<endl;
+                if (!searched[new_search_pos])
+                    this->link_component( component_tree, comp_cnodes_to_pos, new_search_pos, search_pos, searched );
             }
         } else {
             cout<<"search_pos oversize, skipping current linking process.\n";
