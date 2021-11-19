44c40b2887a54a154fbc4b7e6a8cc02ab0312cfd
junhui <17667520168@163.com>
2021-11-19 11:13:08 +0800

    test

--- PUNCH/Filter/G_Graph.cpp
+++ PUNCH/Filter/G_Graph.cpp
@@ -2288,13 +2288,12 @@ void G_Graph::link_component( vector<edge_cncted_comp>& component_tree, map<Node
 //                cout<<"key "<<*chlit<<" not exists\n";
 //                continue;
 //            }
-
+            if (comp_cnodes_to_pos[(*chlit)] > 2008012832)
+                cout<<"chil id: "<<(*chlit)<<endl;
 			if( comp_cnodes_to_pos[(*chlit)] == parent_pos ){
 				component_tree[search_pos].neighbor_id_in_parent = *chlit;
 			}
 			else{
-                if (comp_cnodes_to_pos[(*chlit)] > 2008012832)
-                    cout<<"chil id: "<<(*chlit)<<endl;
 				children_pos.push_back( comp_cnodes_to_pos[(*chlit)] );
 			}
 		}
