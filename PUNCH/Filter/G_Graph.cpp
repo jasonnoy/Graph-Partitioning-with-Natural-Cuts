4a417745a3c4b86ce7f8c9d3e4457aa17cb33995
junhui <17667520168@163.com>
2021-11-25 14:11:55 +0800

    test

--- PUNCH/Filter/G_Graph.cpp
+++ PUNCH/Filter/G_Graph.cpp
@@ -1533,7 +1533,7 @@ void G_Graph::find_natural_cuts( bool natural_cuts[], NodeSize sz_lim ){
 }
 
 NodeID G_Graph::next_center( vector<NodeID>& shuffle_nodes, vector<bool>& node_in_core, NodeID& index ){
-        while(!node_in_core[shuffle_nodes[index]]) {
+        while(node_in_core[shuffle_nodes[index]]) {
             index++;
         }
         node_in_core[index] = true;
