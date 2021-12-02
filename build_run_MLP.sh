ffb8be376357977e4bb8682cf2b41d321bd33c30
junhui <17667520168@163.com>
2021-12-02 18:08:46 +0800

    sequential

--- build_run_MLP.sh
+++ build_run_MLP.sh
@@ -48,8 +48,8 @@ g++ ./MLP/MultiLayerPartition.cpp -g -lpthread ./MLP/Assembly.cpp ./MLP/Filter.c
 
 #./bin/MLP ./data/paras.txt /data/cris.su/code/ch-routing-demo/data/china/nodes /data/cris.su/code/ch-routing-demo/data/china/links ./result/ 50 >> ./logs/"$date_str"/"$time_str".log 2>&1
 
-#./bin/MLP ./data/paras.txt /data/map_data/rp_2.0/normal/2021_11_25_21_07_41/all/all/nodes /data/map_data/rp_2.0/normal/2021_11_25_21_07_41/all/all/links ./result/ 48 >> ./logs/"$date_str"/"$time_str".log 2>&1
+./bin/MLP ./data/paras.txt /data/map_data/rp_2.0/normal/2021_11_25_21_07_41/all/all/nodes /data/map_data/rp_2.0/normal/2021_11_25_21_07_41/all/all/links ./result/ 52 "$date_str$time_str" >> ./logs/"$date_str"/"$time_str".log 2>&1
 
- ./bin/MLP ./data/paras_beijing.txt /data/jjh/mlp_test/Graph-Partitioning-with-Natural-Cuts/data/beijing/binary_data/nodes /data/jjh/mlp_test/Graph-Partitioning-with-Natural-Cuts/data/beijing/binary_data/links ./result/beijing/ 32 "$date_str$time_str" >> ./logs/"$date_str"/"$time_str".log 2>&1
+# ./bin/MLP ./data/paras_beijing.txt /data/jjh/mlp_test/Graph-Partitioning-with-Natural-Cuts/data/beijing/binary_data/nodes /data/jjh/mlp_test/Graph-Partitioning-with-Natural-Cuts/data/beijing/binary_data/links ./result/beijing/ 32 "$date_str$time_str" >> ./logs/"$date_str"/"$time_str".log 2>&1
 
 echo "Finished"
