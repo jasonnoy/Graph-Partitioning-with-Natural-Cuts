b732dbcc293b3a7af848db7b2de72bdcbcd3ee67
junhui <17667520168@163.com>
2021-12-03 14:41:52 +0800

    test only

--- PUNCH/Filter/G_Graph.cpp
+++ PUNCH/Filter/G_Graph.cpp
@@ -875,7 +875,7 @@ void G_Graph::cnt_two_cuts( const vector< vector<EdgeID> >& edge_classes,
 
 		//edge_classes[0] is the class of 1-cut
         cout<<"eqv class num: "<<edge_classes.size()<<endl;
-        const int thread_num = edge_classes.size() < thread_cap ? edge_classes.size() : thread_cap;
+        int thread_num = edge_classes.size() < thread_cap ? edge_classes.size() : thread_cap;
         vector<vector<NodeID>> thread_index(thread_num);
         for (NodeID i = 0; i < edge_classes.size(); i++)
             thread_index[i%thread_num].push_back(i);
