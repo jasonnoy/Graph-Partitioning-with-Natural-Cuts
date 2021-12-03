82b0653d92ba63f75d578f8686027ad697fab082
junhui <17667520168@163.com>
2021-12-03 18:37:45 +0800

    test only

--- PUNCH/Filter/G_Graph.cpp
+++ PUNCH/Filter/G_Graph.cpp
@@ -734,8 +734,11 @@ void static_contract_nodes(vector<NodeID>& node_list, vector<NodeID>& del_cnt_no
         //In case 2012-12-07 Now, I think this is impossible, so remove it
         //if( cnid == new_node_id )
         //	continue;
-        if (cnid == new_node_id)
+        if (cnid == new_node_id) {
+            cout<<"cnid=new_id\n";
             continue;
+        }
+
 
 
         vector<NodeID>::const_iterator cnit = contract_node_list[cnid].begin();
