00e9ae9c090682503128a6535445f47ff8af0c39
junhui <17667520168@163.com>
2021-12-03 13:37:32 +0800

    test only

--- PUNCH/Filter/G_Graph.cpp
+++ PUNCH/Filter/G_Graph.cpp
@@ -744,6 +744,8 @@ void static_contract_nodes(vector<NodeID>& node_list, vector<NodeID>& del_cnt_no
 void parallel_cnt_two_cuts(vector<G_Node>& node_list, const vector<NodeID>& sym_edge_id, vector<G_Edge>& edge_list, vector<NodeID>& contract_to, vector<vector<NodeID>>& contract_node_list, const vector<vector<EdgeID>>& edge_classes, const vector<NodeID>& index, NodeSize sz_lim, vector<NodeID>& del_cnt_node, mutex& m_lock, set<NodeID>& contract_record) {
 
     for(NodeID i : index ){ //deal with one edge class
+        if (contract_node_list.size() >= 2*node_list.size())
+            cout<<"cnt list surpass 2*nodelist size\n";
         vector<NodeID> edge_class_eid = edge_classes[i];
         if( edge_class_eid.size() < 4 ) // needs two edges for a pair of 2-cuts edge
             continue;
