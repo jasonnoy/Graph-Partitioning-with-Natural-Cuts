5ca29146c2b793f5df4bbdd32418154345478a4b
junhui <17667520168@163.com>
2021-12-10 11:25:19 +0800

    debug

--- PUNCH/Filter/G_Graph.cpp
+++ PUNCH/Filter/G_Graph.cpp
@@ -514,11 +514,8 @@ void G_Graph::read_graph( const vector<NodeID>& nodes, const vector<vector<NodeI
     for (G_Edge edge : edge_list) {
         NodeID sid = edge.get_source();
         for (auto e_ptr : node_list[sid].get_adj_list()) {
-            NodeID sid = edge.get_source();
-            for (auto e_ptr : node_list[sid].get_adj_list()) {
-                if (e_ptr->get_target() == sid) {
-                    sym_id[edge.get_id()] = e_ptr->get_id();
-                }
+            if (e_ptr->get_target() == sid) {
+                sym_id[edge.get_id()] = e_ptr->get_id();
             }
         }
     }
