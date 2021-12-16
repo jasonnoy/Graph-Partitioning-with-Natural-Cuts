3f6bf5992912b0327f29de16fd4025411d173f14
junhui <17667520168@163.com>
2021-12-16 14:49:49 +0800

    check read in mem

--- build_run_MLP.sh
+++ build_run_MLP.sh
@@ -54,10 +54,10 @@ g++ ./MLP/MultiLayerPartition.cpp -g -lpthread ./MLP/Assembly.cpp ./MLP/Filter.c
 #./bin/MLP ./data/paras.txt /data/jjh/partitions/1207/topo_node.hllnavi /data/jjh/partitions/1207/topo_link.hllnavi /data/jjh/partitions/1207/crp_weight_dist.hllnavi ./result/ 52 "$date_str$time_str" >> ./logs/"$date_str"/"$time_str".log 2>&1
 
 # topo china test
-#./bin/MLP ./data/paras.txt ./result/ 52 000 /data/jjh/crp_data/china/crp_weight_dist.hllnavi
+./bin/MLP ./data/paras.txt ./result/ 52 000 /data/jjh/crp_data/china/crp_weight_dist.hllnavi
 
 # topo china input
-./bin/MLP ./data/paras.txt ./result/ 52 "$date_str$time_str" /data/jjh/crp_data/china/crp_weight_dist.hllnavi >> ./logs/"$date_str"/"$time_str".log 2>&1
+#./bin/MLP ./data/paras.txt ./result/ 52 "$date_str$time_str" /data/jjh/crp_data/china/crp_weight_dist.hllnavi >> ./logs/"$date_str"/"$time_str".log 2>&1
 
 # Topo beijing input
 #./bin/MLP ./data/paras_beijing.txt ./result/beijing/ 32 "$date_str$time_str" /data/jjh/mlp_test/Graph-Partitioning-with-Natural-Cuts/data/beijing/binary_data/crp_weight_dist.hllnavi >> ./logs/"$date_str"/"$time_str".log 2>&1
