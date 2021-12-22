219c46a1e4169f269f673cfe16ad02ef9e21a4d6
junhui <17667520168@163.com>
2021-12-22 11:31:11 +0800

    debug

--- MLP/GraphPrinter.h
+++ MLP/GraphPrinter.h
@@ -46,7 +46,7 @@ private:
 
 public:
     GraphPrinter(const vector<vector<NodeID>>& in_result, const vector<vector<NodeID>>& in_id_map, const vector<NodeID>& real_map, vector<EdgeID>& cellEdges, const int u, const int l, const bool contract): a_result(in_result), id_map(in_id_map), real_map(real_map), cell_edges(cellEdges), U(u), l(l), contract_tiny(contract), void_nodes(replace_vec){}
-    GraphPrinter(const vector<NodeID>& void_nodes, vector<EdgeID>& cellEdges): void_nodes(void_nodes), cell_edges(cellEdges), id_map(replace_vec_vec), real_map(replace_vec), a_result(replace_vec_vec){}
+    GraphPrinter(const vector<NodeID>& void_nodes, vector<EdgeID>& cellEdges): void_nodes(void_nodes), cell_edges(cellEdges), id_map(replace_vec_vec), real_map(replace_vec), a_result(replace_vec_vec), contract_tiny(false){}
     void write_void_result(mutex& w_lock, vector<vector<NodeID>>& res_cells_nodes, vector<vector<NodeID>>& res_cells_edges);
     void write_MLP_result(mutex& w_lock, vector<vector<NodeID>>& res_cells_nodes, vector<vector<NodeID>>& res_cells_edges, vector<vector<NodeID>>& res_void_cells);
 //    NodeID nodes_result_size(){return result_nodes.size();}
