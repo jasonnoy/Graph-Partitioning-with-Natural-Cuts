80882cb3954f92d4d5abc5071ddfbadabccdc79c
junhui <17667520168@163.com>
2021-12-30 00:02:49 +0800

    accumulate topo input

--- App/MultiLayerPartition.cpp
+++ App/MultiLayerPartition.cpp
@@ -51,7 +51,7 @@ int main(int argc, char** argv) {
     }
 
     bool accumulate = true;
-    const string partition_file_path = "/data/jjh/partitions/1220/node_partitions_20211222113230.txt";
+    const string partition_file_path = "/data/jjh/partitions/1222/node_partitions_20211222113230.txt";
 //    const int ori_layer = 5;
     const int base_layer = 3;
     if (accumulate) {
